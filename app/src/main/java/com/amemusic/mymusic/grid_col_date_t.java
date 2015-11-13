b1055a6601247e5f7295f45c24f41be6b2455742
Kari Lentz <karilentz@att.net>
2015-11-12 22:30:43 -0500

    default format for date column

--- app/src/main/java/com/amemusic/mymusic/grid_col_date_t.java
+++ app/src/main/java/com/amemusic/mymusic/grid_col_date_t.java
@@ -10,12 +10,19 @@ import java.text.SimpleDateFormat;
 
 public class grid_col_date_t extends grid_col_t{
 
+    private String default_format_ = "MM/dd/yyyy";
+
     grid_col_date_t(Context context, String key, String header, int width){
         super(context, key, header, width, types_t.DATE);
     }
 
+    grid_col_date_t default_format(String value){
+        default_format_ = value;
+        return this;
+    }
+
     @Override
     public String string(media_t media){
-        return new SimpleDateFormat("MM/dd/yyyy").format(media.get_data(key_));
+        return new SimpleDateFormat(default_format_).format(media.get_data(key_));
     }
 }
